bc66c86271cd1ab54a3fee06013da9086eb144c1
thomasgoulet <thomas@jfgoulet.com>
2022-12-03 20:12:07 -0500

    Add gitignore

--- selenium/SeleniumTests.cs
+++ selenium/SeleniumTests.cs
@@ -1,12 +1,33 @@
+using System;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using Xunit;
 
 namespace selenium;
 
-public class UnitTest1
+        
+        
+
+public class SeleniumTests : IDisposable
 {
+
+    IWebDriver Driver;
+
+    public SeleniumTests() 
+    {
+        Driver = new ChromeDriver();
+        Driver.Navigate().GoToUrl(Path.GetFullPath("../webapp/index.html"));
+    }
+    
+    public void Dispose()
+    {
+        Driver.Dispose();
+    }
+    
     [Fact]
     public void Test1()
     {
-
+        Console.WriteLine(Driver.Title);
     }
 }

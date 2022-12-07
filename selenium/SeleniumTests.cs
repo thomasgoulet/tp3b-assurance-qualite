b94de42c26659b531f2bef31e58dcbb856e3cc25
Thomas Goulet <thomas@jfgoulet.com>
2022-12-07 17:01:20 -0500

    Working with Chrome 108 installed

--- selenium/SeleniumTests.cs
+++ selenium/SeleniumTests.cs
@@ -2,13 +2,9 @@ using System;
 using System.IO;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using WebDriverManager.DriverConfigs.Impl;
 using Xunit;
 
 namespace selenium;
-
-        
-        
 
 public class SeleniumTests : IDisposable
 {
@@ -17,9 +13,8 @@ public class SeleniumTests : IDisposable
 
     public SeleniumTests() 
     {
-        new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig());
-        Driver = new ChromeDriver();
-        Driver.Navigate().GoToUrl(Path.GetFullPath("../webapp/index.html"));
+        Driver = new ChromeDriver("./driver");
+        Driver.Navigate().GoToUrl("https://tp3b-assurance.tgoulet.com");
     }
     
     public void Dispose()
@@ -31,5 +26,6 @@ public class SeleniumTests : IDisposable
     public void Test1()
     {
         Console.WriteLine(Driver.Title);
+        System.Threading.Thread.Sleep(1000000);
     }
 }
